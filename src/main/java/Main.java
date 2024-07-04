2565470b17741fbe6b10cf85f5ef3f6b242c6030
pedroferens <169068429+pedroferens@users.noreply.github.com>
2024-07-04 10:58:06 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+  import java.util.Scanner;
+  
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("escreva seu nome");
+     String nome = sc.nextLine();
+     System.out.println("quantos anos voce tem?");
+     String idade = sc.nextLine();
+     System.out.println("qual sua altua?");
+     String altura = sc.nextLine();
+     System.out.println("qual seu sexo");
+    System.out.println( "se for masculino digite 1 se for femenino digite 2");
+    
+
+    System.out.println("Olá" + nome +, "fizemos uma breve analise das informações passadas e temos as seguintes informações a repassar":");
+
+    
+    
+    
+    
+  }
+
+ 
+}
